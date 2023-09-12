cc8ad641d50f65bf8d8fdd16ee741b96d64c8651
Raphael <raphael.nicolle@etu.unicaen.fr>
2023-09-12 13:34:08 +0200

    decoupage_diminuerTemps

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const activitee = document.getElementById("activitee");
 const timerElement = document.getElementById("timer");
+const pause = 5;
 const depart = 25;
-let temps = 25*60-1;
-setInterval(diminuerTemps, 1000);
+let temps = depart*60;
+let minutes = parseInt(temps / 60, 10);
+let secondes = parseInt(temps % 60, 10);
+const bouton = document.getElementById("start");
+bouton.addEventListener("click", () =>{
+    
+    DemarrerTimer();
+});
 
-function diminuerTemps(){
-    let minutes = parseInt(temps / 60, 10);
-    let secondes = parseInt(temps % 60, 10);
 
+function DemarrerTimer(){
+    temps--;
+    setInterval(diminuerTemps, 1000);
+}
+
+function affichageTimer(minutes, secondes){
     if (minutes<10){
         minutes = "0"+minutes;
     }
@@ -15,11 +25,21 @@ function diminuerTemps(){
         secondes = "0"+secondes;
     }
 
+    timerElement.innerText= `${minutes} : ${secondes}`;
+
+}
+
+function diminuerTemps(){
+
+    minutes = parseInt(temps / 60,10) ;
+    secondes = parseInt(temps % 60,10);
+
+    affichageTimer(minutes,secondes);
+
+    temps--;
     if(temps <= 0){
-        temps = 5*60+1;
+        temps = pause*60;
         activitee.innerText = "Pause";
     }
 
-    timerElement.innerText= `${minutes} : ${secondes}`;
-    temps--;
 }
